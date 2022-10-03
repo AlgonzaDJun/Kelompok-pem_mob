45e6b3e4b5e0756aa132c47331a7f840fddc44f9
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-10-03 08:40:45 +0700

    first commit

---
+++ login_signup/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:login_signup/homepage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+
